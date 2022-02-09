14768e7872d087103d63f0a370ecb0223cf163c7
Esteban <jomariux4@gmail.com>
2022-02-08 22:07:35 -0600

    Add playRound function

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function computerPlay() {
     options = ['Rock', 'Paper', 'Scissors'];
     return options[Math.floor(Math.random() * 3)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It\'s a draw. ${playerSelection} against ${computerSelection} is equal.`
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return 'You win! Scissors beats Paper.';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return 'You win! Paper beats Rock.';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You win! Rock beats Scissors.';
+    } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
+        return 'You lose! Scissors beats Paper.';
+    } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
+        return 'You lose! Paper beats Rock.';
+    } else if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
+        return 'You lose! Rock beats Scissors.';
+    } else return 'An error has ocurred';
+}
