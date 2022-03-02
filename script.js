abc7beeaa9f728fb019dd7f68e95f4ed716aa51f
Esteban <jomariux4@gmail.com>
2022-03-01 19:00:43 -0600

    Add complete UI

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const rockButton = document.querySelector('rock');
+const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
 const content = document.querySelector('.content');
-rockButton.addEventListener('click', playRound('rock', computerPlay));
-paperButton.addEventListener('click', playRound('paper', computerPlay));
-scissorsButton.addEventListener('click', playRound('scissors', computerPlay));
+const playButton = document.querySelector('.playButton');
+const score = document.querySelector('.score');
+const container = document.querySelector('.container');
+const buttons = document.querySelector('.buttons');
+let userPoints = 0;
+let computerPoints = 0;
+let result = document.createElement('div');
+
+rockButton.addEventListener('click', function() {playRound('rock', computerPlay());});
+paperButton.addEventListener('click', function () {playRound('paper', computerPlay());});
+scissorsButton.addEventListener('click', function () {playRound('scissors', computerPlay());});
+score.textContent = `User points: ${userPoints}.
+Computer points: ${computerPoints}.`
+content.appendChild(result);
+result.classList.add('result')
 
 function computerPlay() {
     options = ['rock', 'paper', 'scissors'];
@@ -13,61 +25,97 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        const result = document.createElement('div');
         result.textContent = `It\'s a draw. ${playerSelection} against ${computerSelection} is equal.`
-        content.appendChild(result);
-        return undefined;
+        setTimeout(() => result.textContent = '', 3000);
+        verificateWinner();
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You win! Scissors beats Paper.';
+        result.textContent = 'You win! Scissors beats Paper.';
+        userPoints++;
+        setTimeout(() => result.textContent = '', 3000);
+        score.textContent = `User points: ${userPoints}.
+        Computer points: ${computerPoints}.`;
+        verificateWinner();
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You win! Paper beats Rock.';
+        result.textContent = 'You win! Paper beats Rock.';
+        userPoints++;
+        setTimeout(() => result.textContent = '', 3000);
+        score.textContent = `User points: ${userPoints}.
+        Computer points: ${computerPoints}.`;
+        verificateWinner();
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You win! Rock beats Scissors.';
+        result.textContent = 'You win! Rock beats Scissors.';
+        userPoints++;
+        setTimeout(() => result.textContent = '', 3000);
+        score.textContent = `User points: ${userPoints}.
+        Computer points: ${computerPoints}.`
+        verificateWinner();
     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        return 'You lose! Scissors beats Paper.';
-    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        return 'You lose! Paper beats Rock.';
+        result.textContent = 'You lose! Scissors beats Paper.'; 
+        computerPoints++;
+        setTimeout(() => result.textContent = '', 3000);
+        score.textContent = `User points: ${userPoints}.
+        Computer points: ${computerPoints}.`;
+        verificateWinner();
+    }  else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        result.textContent = 'You lose! Paper beats Rock.';
+        computerPoints++;
+        setTimeout(() => result.textContent = '', 3000);
+        score.textContent = `User points: ${userPoints}.
+        Computer points: ${computerPoints}.`;
+        verificateWinner();
     } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        return 'You lose! Rock beats Scissors.';
-    } else return 'An error has ocurred';
-}
-
-function game() {
-    for (;userPoints < 5 && computerPoints < 5;) {
-        let userPoints = 0;
-        let computerPoints = 0;
-        let computerSelection = computerPlay();
-        selectionReport = document.createElement('div')
-        selectionReport.textContent = `You selected ${playerSelection} and the computer selected ${computerSelection}.`
-        content.appendChild(selectionReport);
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        addPoints(result);        
+        result.textContent = 'You lose! Rock beats Scissors.';
+        computerPoints++;
+        setTimeout(() => result.textContent = '', 3000);
+        score.textContent = `User points: ${userPoints}.
+        Computer points: ${computerPoints}.`;
+        verificateWinner();
     }
-
-    console.log(`You made ${userPoints} points and the computer made ${computerPoints}.`)
-
-    if (userPoints > computerPoints) {
-        return 'You\'ve won! Congratulations';
-    } else if (userPoints < computerPoints) {
-        return 'You\'ve lost! Better luck next time';
-    } else return 'It\'s a draw';
 }
 
-function addPoints(result) {
-    if (result.slice(0, 7) === 'You win') {
-        return userPoints++;
-    } else if (result.slice(0, 8) === 'You lose') {
-        return computerPoints++;
-    } else return;
+function verificateWinner() {
+    if (userPoints === 5 && userPoints > computerPoints) {
+        let winnerReport = document.createElement('div');
+        winnerReport.textContent = 'You\'ve won! Congratulations';
+        container.insertBefore(winnerReport, content);
+        setTimeout(() => container.removeChild(winnerReport), 5000);
+        content.removeChild(buttons);
+        container.removeChild(score);
+        userPoints = 0;
+        computerPoints = 0;
+        let playAgainButton = document.createElement('button');
+        playAgainButton.textContent = 'Play again';
+        playAgainButton.classList.add('playButton');
+        content.appendChild(playAgainButton);
+        playAgainButton.addEventListener('click', () => {
+            content.removeChild(playAgainButton);
+            content.appendChild(buttons);
+            container.insertBefore(score, content);
+        })
+        
+        score.textContent = `User points: ${userPoints}.
+        Computer points: ${computerPoints}.`;
+        return true;
+    } else if (computerPoints === 5 && computerPoints > userPoints) {
+        let winnerReport = document.createElement('div');
+        winnerReport.textContent = 'You\'ve lost! Better luck next time';
+        container.insertBefore(winnerReport, content);
+        setTimeout(() => container.removeChild(winnerReport), 5000);
+        content.removeChild(buttons);
+        container.removeChild(score);
+        userPoints = 0;
+        computerPoints = 0;
+        let playAgainButton = document.createElement('button');
+        playAgainButton.textContent = 'Play again';
+        playAgainButton.classList.add('playButton');
+        content.appendChild(playAgainButton);
+        playAgainButton.addEventListener('click', () => {
+            content.removeChild(playAgainButton);
+            content.appendChild(buttons);
+            container.insertBefore(score, content);
+        })
+        score.textContent = `User points: ${userPoints}.
+        Computer points: ${computerPoints}.`;
+        return true;
+    } else return false;
 }
-
-function verifyPlayerSelection(playerSelection) {
-    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') return true;
-    else return false;
-}
-
-
-
-
-
